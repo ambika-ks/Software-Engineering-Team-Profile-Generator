c471ef70ea72cd8b1c5261f052a3044603c09504
Ambika Kochuparambil Sidharthan <ak21ace@herts.ac.uk>
2023-02-17 12:43:33 +0000

    Aded basic queries to create a team

--- index.js
+++ index.js
@@ -7,10 +7,47 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-console.log(OUTPUT_DIR);
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+
+//Array for team members
+teamMembersArray = [];
+
+//Start
+function startApp(){
+    createManager();
+
+    //Adding Team Members
+    function makeTeam()
+    {
+        inquirer.prompt([
+            
+            {
+                type: "list",
+                message: "Enter Team Member Positions",
+                name: "addTeamMember",
+                choices: ["Engineer", "Intern", "No more Team Member"]
+
+            }]) .then(function (res) {
+                switch (res.addTeamMember) {
+                    case "Engineer":
+                        createEngineer();
+                        break;
+                    case "Intern":
+                        createIntern();
+                        break;
+                    default:
+                        buildHTML();
+                }
+            });
+
+        
+
+        
+    }
+}
